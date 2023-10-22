02d326d9cd947a2eaa23189c9f8bc2a56ad47f8a
kanishkasubash <kanishkasubash@gmail.com>
2023-10-23 00:25:44 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/index.js 	modified:   webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!!!")
+var marks = {
+    maths: 80,
+    science: 75,
+    english: 60
+};
+
+var grade = {
+    ...marks,
+    grade: "pass"
+};
+
+console.log(grade);
--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,28 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'), // Output directory
         filename: 'main.js', // Output bundle filename
     },
-
+    module: {
+        rules: [
+            {
+                test: /\.js$/, // Match JavaScript files
+                exclude: /node_modules/, // Exclude node_modules
+                use: {
+                    loader: 'babel-loader', // Use Babel for JavaScript files
+                    options: {
+                        presets: [
+                            ['@babel/preset-env']
+                        ]
+                    }
+                }
+            }
+        ]
+    },
     devtool: false,
     devServer: {
         static: {
-            directory: path.join(__dirname, 'dist'),
+            directory: path.join(__dirname, 'dist'), // Serve content from the 'dist' directory
         },
         compress: true,
-        port: 3000,
+        port: 3000, // Port for development server
     },
 };
