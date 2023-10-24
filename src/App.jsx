e77fe183019867843d8dd0a17baab4eb144ea241
kanishkasubash <kanishkasubash@gmail.com>
2023-10-24 23:19:22 +0530

    modified:   src/App.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import webpackLogo from './assets/webpack-logo.svg';
-import webpackChart from './assets/webpack-chart.png';
+import webpackLogo from "./assets/webpack-logo.svg";
+import webpackChart from "./assets/webpack-chart.png";
 
 const App = () => {
-    const [img, setImg] = useState({
+    const [img] = useState({
         logo: webpackLogo,
         heroImg: webpackChart
     });
@@ -12,19 +12,21 @@ const App = () => {
             <main className="container mx-auto">
                 <div className="flex flex-row justify-center py-5">
                     <img
-                        src={img.logo}
+                        src={webpackLogo}
                         alt="webpack-logo"
                         width={200}
                         height={100}
                     />
                 </div>
-                <h1 className="text-4xl text-malibu text-center">Starter Template</h1>
+                <h1 className="text-4xl text-center text-malibu">Starter Template</h1>
                 <div className="flex flex-row justify-center py-5">
                     <img
                         src={img.heroImg}
                         alt="webpack-chart"
                     />
                 </div>
+                <p>Webpack 5 is a popular open-source JavaScript module bundler that is widely used in modern web development. It is an evolution of earlier versions of Webpack, and it brings several new features and improvements over its predecessors.</p>
+                <p>It's worth noting that Webpack is highly configurable, and its configuration can be complex for larger projects. To make it more accessible, there are tools and boilerplates like Create React App and Vue CLI that abstract much of the configuration away and provide a streamlined development experience.</p>
             </main>
         </>
     );
