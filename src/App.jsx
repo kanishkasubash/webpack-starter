fabbb549bfacf0aca76660106d1c074d18843fd8
kanishkasubash <kanishkasubash@gmail.com>
2023-10-26 16:52:28 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	modified:   src/index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
-import SimpleImageSlider from "react-simple-image-slider";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 import { webpackLogo } from './assets';
-import { sliderImages } from './constants'
+import { sliderImages } from './constants';
 
 const App = () => {
     return (
         <>
-            <main className="container mx-auto">
-                <div className="flex flex-row justify-center py-5">
+            <main className="container grid justify-center grid-cols-1 mx-auto">
+                <div className="flex flex-row justify-center my-5">
                     <img
                         src={webpackLogo}
                         alt="webpack-logo"
                         width={200}
                         height={100}
                     />
-                </div>
+                </div>                
                 <h1 className="text-4xl text-center text-malibu">Starter Template</h1>
-                <div className="flex flex-row justify-center py-5">
-                    <SimpleImageSlider
-                        width={1080}
-                        height={550}
-                        images={sliderImages}
-                        showBullets={false}
-                        showNavs={false}
-                        loop={true}
-                        autoPlay={true}
-                    />
+                <div className="flex flex-row justify-center my-5">
+                    <Carousel autoPlay={true} infiniteLoop={true} showStatus={false} showThumbs={false} showArrows={false} className=" lg:w-[70%]" >
+                        {sliderImages.map((image, index) => (
+                            <div key={index}>
+                                <img src={image.url} />
+                            </div>
+                        ))}
+                    </Carousel>
                 </div>
                 <p>Webpack 5 is a popular open-source JavaScript module bundler that is widely used in modern web development. It is an evolution of earlier versions of Webpack, and it brings several new features and improvements over its predecessors.</p>
                 <p>It's worth noting that Webpack is highly configurable, and its configuration can be complex for larger projects. To make it more accessible, there are tools and boilerplates like Create React App and Vue CLI that abstract much of the configuration away and provide a streamlined development experience.</p>
